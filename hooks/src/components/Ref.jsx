eb6993544eae78b2be014b3435c044f097e319f5
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-29 10:35:19 +0530

    normal

--- hooks/src/components/Ref.jsx
+++ hooks/src/components/Ref.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 
 export const Ref = () => {
-  const [first, setFirst] = useState(0);
   const intervalRef = useRef(null); // Create a ref to hold the interval ID
 
   useEffect(() => {
     intervalRef.current = setInterval(() => {
-      setFirst((prevFirst) => prevFirst + 1); // Correctly update the state
+      // This function runs every second
+      console.log("Interval running");
     }, 1000);
 
     // Cleanup function to clear the interval when the component unmounts
@@ -15,10 +15,5 @@ export const Ref = () => {
     };
   }, []); // Empty dependency array means this effect runs once after the initial render
 
-  return (
-    <div>
-      My Component
-      <div>Count: {first}</div> {/* Display the current count */}
-    </div>
-  );
+  return <div>My Component</div>;
 };
